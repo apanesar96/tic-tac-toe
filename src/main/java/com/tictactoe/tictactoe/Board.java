e61189ac43b81ba0a6059ef3060e209f48cbe104
Aman Panesar <amanpanesar@Amans-MacBook-Pro.local>
2021-09-08 15:38:57 +0100

    refactor game into board

--- src/main/java/com/tictactoe/tictactoe/Board.java
+++ src/main/java/com/tictactoe/tictactoe/Board.java
@@ -1,8 +1,10 @@
 package com.tictactoe.tictactoe;
 
+import java.lang.invoke.WrongMethodTypeException;
+
 public class Board {
 
-    char [][] gameBoard = {{'.', '.', '.'}, {'.', '.', '.'}, {'.', '.', '.'}};
+    private char [][] gameBoard = {{'.', '.', '.'}, {'.', '.', '.'}, {'.', '.', '.'}};
 
     public String printBoard() {
         StringBuilder boardOutput = new StringBuilder();
@@ -16,9 +18,31 @@ public class Board {
         return boardOutput.toString();
     }
 
-    public void placeToken(Position position, char activePlayer) {
+    public void placeToken(Position position, char activePlayer) throws Exception {
+        if (!isPositionAvailable(position)) throw new Exception("That position is already filled.");
         int x = position.x;
         int y = position.y;
         gameBoard[y][x] = activePlayer;
     }
+
+    public boolean isTie() {
+        boolean isTie = true;
+
+        for ( char[] row : gameBoard) {
+            for (char column : row) {
+                if(column == '.'){
+                    isTie = false;
+
+                    break;
+                }
+            }
+        }
+
+        return isTie;
+    }
+
+    private boolean isPositionAvailable(Position position) {
+        return gameBoard[position.y][position.x] == '.';
+    }
+
 }
--- src/main/java/com/tictactoe/tictactoe/TicTacToe.java
+++ src/main/java/com/tictactoe/tictactoe/TicTacToe.java
@@ -14,35 +14,13 @@ public class TicTacToe {
     private Board gameBoard = new Board();
 
     public String playTurn(Position position) throws Exception {
-        if (!isPositionAvailable(position)) throw new Exception("That position is already filled.");
-
         gameBoard.placeToken(position, activePlayer);
 
-        if (isTie()) return "Tie";
+        if (gameBoard.isTie()) return "Tie";
 
         changeActivePlayer();
 
         return gameBoard.printBoard();
-    }
-
-    private boolean isTie() {
-        boolean isTie = true;
-
-        for ( char[] row : gameBoard) {
-            for (char column : row) {
-                if(column == '.'){
-                    isTie = false;
-
-                    break;
-                }
-            }
-        }
-
-        return isTie;
-    }
-
-    private boolean isPositionAvailable(Position position) {
-       return gameBoard[position.y][position.x] == '.';
     }
 
     private void changeActivePlayer() {
